279ee4d4fd9e887494016428884473a5123c898c
Trịnh Hiệp <90686182+hieptrinh2002@users.noreply.github.com>
2023-11-27 22:00:38 +0700

    loginPage
    add login page

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes as ReactRouterDomRoutes } from "react-router-dom";
 import Landing from "./pages/Landing";
+import Login from "./pages/Login";
 
 function Routes() {
   return (
     <ReactRouterDomRoutes>
       <Route path="/" element={<Landing />} />
+      <Route path="/Login" element={<Login />} />
     </ReactRouterDomRoutes>
   );
 }
