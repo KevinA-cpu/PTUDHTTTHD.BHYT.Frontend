602528b2165b4d98a65886cbf6db6efcfd3091ae
Trịnh Hiệp <90686182+hieptrinh2002@users.noreply.github.com>
2023-11-27 23:28:35 +0700

    add register page

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes as ReactRouterDomRoutes } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function Routes() {
   return (
     <ReactRouterDomRoutes>
       <Route path="/" element={<Landing />} />
       <Route path="/Login" element={<Login />} />
+      <Route path="/Register" element={<Register />} />
     </ReactRouterDomRoutes>
   );
 }
