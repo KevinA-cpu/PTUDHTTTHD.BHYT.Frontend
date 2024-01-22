fa048ae5bdbcca2a94ba720b1c82e8f04be5709b
Heisenburg <69845869+KevinA-cpu@users.noreply.github.com>
2024-01-22 14:16:57 +0700

    tests: update test

--- src/tests/pages/OTP.test.tsx
+++ src/tests/pages/OTP.test.tsx
@@ -1,8 +1,13 @@
 import { test, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import OTP from "../../pages/OTP";
+import { MemoryRouter } from "react-router-dom";
 
 test("OTP mounts properly", () => {
-  render(<OTP />);
+  render(
+    <MemoryRouter>
+      <OTP />
+    </MemoryRouter>
+  );
   expect(screen.getByText("Xác thực OTP")).toBeInTheDocument();
 });
